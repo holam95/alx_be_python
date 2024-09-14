9cdc2b462f59c1a36ee2fa5f45be20f895b5b8cc
holam95 <holambis@outlook.com>
2024-09-14 07:51:58 -0700

    added basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+result1= number1 - number2
+result2 = number1 * number2
+print("Addition of", number1 "and", number2, is, result)
+print("Suntraction of", number1 "and", number2, is, result)
+print("Multiplication of", number1 "and", number2, is, result)
