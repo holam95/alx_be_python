12ef3003a95569e60929231226a3accdf46a9a25
holam95 <holambis@outlook.com>
2024-10-14 15:46:23 -0700

    changes

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -23,4 +23,4 @@ class BankAccount:
 
     def display_balance(self):
         """Display the current account balance in a user-friendly format."""
-        print("Current balance:" ,self.__account_balance:.2f)
+        print("Current Balance:" ,self.__account_balance:.2f)
