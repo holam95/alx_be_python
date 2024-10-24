8d9918190d537c692a159cfcc9cb5699522e0cd3
holam95 <holambis@outlook.com>
2024-10-24 09:07:17 -0700

    added changes

--- oop/library_system.py
+++ oop/library_system.py
@@ -24,7 +24,7 @@ class PrintBook(Book):
         self.page_count = page_count
 
     def __str__(self):
-        return f"PrintBook: {self.title} by {self.author} Page count: {self.page_count} pages"
+        return f"PrintBook: {self.title} by {self.author} Page Count: {self.page_count}"
 
 
 # Library class to manage books (composition)
